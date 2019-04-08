9b4b540964e3fbcb4bb6a2baf6d6456baf25b074
Valerii Sovytskyi <lhubertl@outlook.com>
2019-04-08 10:45:05 +0300

    Reworked blurring algoritm and reworked radial menu view background

---
+++ SpecialTestApp/Helpers/RenderScriptBlur.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Android.Content;
+using Android.Graphics;
+using Android.Renderscripts;
+
+namespace SpecialTestApp.Helpers
+{
+    internal class RenderScriptBlur : IDisposable
+    {
+        private readonly RenderScript _renderScript;
+        private readonly ScriptIntrinsicBlur _blurScript;
+
+        private int _lastBitmapWidth = -1;
+        private int _lastBitmapHeight = -1;
+
+        private Allocation _outAllocation;
+
+        public RenderScriptBlur(Context context)
+        {
+            _renderScript = RenderScript.Create(context);
+            _blurScript = ScriptIntrinsicBlur.Create(_renderScript, Element.U8_4(_renderScript));
+        }
+
+        /// <summary>
+        /// Blur bitmap
+        /// </summary>
+        /// <param name="bitmap">Bitmap to be blurred</param>
+        /// <param name="blurRadius">from 1 and up to 25</param>
+        /// <returns></returns>
+        public Bitmap Blur(Bitmap bitmap, float blurRadius)
+        {
+            if (blurRadius < 1f && blurRadius > 25f)
+            {
+                throw new ArgumentOutOfRangeException(nameof(blurRadius), "Blur radius must be in range 1..25");
+            }
+
+            //Allocation will use the same backing array of pixels as bitmap if created with USAGE_SHARED flag
+            var inAllocation = Allocation.CreateFromBitmap(_renderScript, bitmap);
+
+            if (!CanReuseAllocation(bitmap))
+            {
+                _outAllocation?.Destroy();
+
+                _outAllocation = Allocation.CreateTyped(_renderScript, inAllocation.Type);
+                _lastBitmapWidth = bitmap.Width;
+                _lastBitmapHeight = bitmap.Height;
+            }
+
+            _blurScript.SetRadius(blurRadius);
+            _blurScript.SetInput(inAllocation);
+
+            //do not use inAllocation in forEach. it will cause visual artifacts on blurred Bitmap
+            _blurScript.ForEach(_outAllocation);
+            _outAllocation.CopyTo(bitmap);
+
+            inAllocation.Destroy();
+            return bitmap;
+        }
+
+        private bool CanReuseAllocation(Bitmap bitmap)
+        {
+            return bitmap.Height == _lastBitmapHeight && bitmap.Width == _lastBitmapWidth;
+        }
+
+        public void Dispose()
+        {
+            _blurScript.Destroy();
+            _renderScript.Destroy();
+            _outAllocation?.Destroy();
+        }
+    }
+}
--- SpecialTestApp/Views/RadialMenuView.cs
+++ SpecialTestApp/Views/RadialMenuView.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Android.App;
+using Android.Graphics;
 using Android.Graphics.Drawables;
 using Android.OS;
 using Android.Runtime;
@@ -12,21 +13,15 @@ using SpecialTestApp.Helpers;
 
 namespace SpecialTestApp.Views
 {
-    [MvxDialogFragmentPresentation]
+    [MvxFragmentPresentation]
     [Register(nameof(RadialMenuView))]
-    internal sealed class RadialMenuView : MvxDialogFragment<RadialMenuViewModel>
+    internal class RadialMenuView : MvxFragment<RadialMenuViewModel>
     {
         public RadialMenuView()
         {
-            SetStyle(DialogFragmentStyle.NoFrame, Resource.Style.RadialFragmentDialogStyle);
-        }
 
-        private RadialMenuView(IntPtr javaReference, JniHandleOwnership transfer)
-            : base(javaReference, transfer)
-        {
-            SetStyle(DialogFragmentStyle.NoFrame, Resource.Style.RadialFragmentDialogStyle);
         }
-
+        
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
             base.OnCreateView(inflater, container, savedInstanceState);
@@ -56,10 +51,73 @@ namespace SpecialTestApp.Views
 
         private void SetBlurBackground(View view)
         {
-            var screenShot = BlurImageHelper.TakeScreenShot(Activity);
-            var blur = BlurImageHelper.FastBlur(screenShot, 10);
-            var drawable = new BitmapDrawable(Resources, blur);
-            view.Background = drawable;
+            // Take screen shot of fragment background
+            var internalBitmap = TakeScreenShot(Activity);
+
+            // Create workbench
+            var internalCanvas = new Canvas(internalBitmap);
+
+            //Reduce the bitmap in 8 times, to improve faster blurring
+            SetupInternalCanvasMatrix(Activity.Window.DecorView, internalCanvas);
+
+            using (var renderScriptBlur = new RenderScriptBlur(Context))
+            {
+                // Blur the bitmap
+                renderScriptBlur.Blur(internalBitmap, 25);
+
+                // Setup the view background
+                view.Background = new BitmapDrawable(Resources, internalBitmap);
+            }
+        }
+
+        /// <summary>
+        /// Reduce the bitmap in 8 times, to improve faster blurring
+        /// </summary>
+        private void SetupInternalCanvasMatrix(View blurView, Canvas internalCanvas)
+        {
+            var scaleFactor = 8f;
+            
+            var frame = new Rect();
+            Activity.Window.DecorView.GetWindowVisibleDisplayFrame(frame);
+
+            var scaledLeftPosition = -blurView.Left / scaleFactor;
+            var scaledTopPosition = -blurView.Top / scaleFactor;
+
+            var scaledTranslationX = blurView.TranslationX / scaleFactor;
+            var scaledTranslationY = blurView.TranslationY / scaleFactor;
+
+            internalCanvas.Translate(scaledLeftPosition - scaledTranslationX, scaledTopPosition - scaledTranslationY);
+            var scaleX = blurView.ScaleX / scaleFactor;
+            var scaleY = blurView.ScaleY / scaleFactor;
+            internalCanvas.Scale(scaleX, scaleY);
+        }
+
+        /// <summary>
+        /// Take screen shot of activity with removed toolbar
+        /// </summary>
+        /// <param name="activity">Target</param>
+        /// <returns></returns>
+        public Bitmap TakeScreenShot(Activity activity)
+        {
+            var view = activity.Window.DecorView;
+            view.DrawingCacheEnabled = true;
+            view.BuildDrawingCache();
+
+            var frame = new Rect();
+            activity.Window.DecorView.GetWindowVisibleDisplayFrame(frame);
+
+            var cachedBitmap = view.DrawingCache;
+            var statusBarHeight = frame.Top;
+
+            var display = activity.WindowManager.DefaultDisplay;
+            var size = new Point();
+            display.GetSize(size);
+            var width = size.X;
+            var height = size.Y;
+
+            var bitmap = Bitmap.CreateBitmap(cachedBitmap, 0, statusBarHeight, width, height - statusBarHeight);
+            view.DestroyDrawingCache();
+            return bitmap;
         }
     }
 }
